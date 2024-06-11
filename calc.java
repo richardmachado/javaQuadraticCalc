2ed90a67305dafe0bbbe8b138e036235c8637351
Richard Machado <rich92105@gmail.com>
2024-06-10 19:56:19 -0700

    Update calc.java
    some reformatting and combining code

--- calc.java
+++ calc.java
@@ -1,45 +1,31 @@
 import java.util.Scanner;
 
-
-public class MyProgram
+public class file
 {
     public static void main(String[] args)
     {
-       Scanner scanner = new Scanner(System.in);
-        
-       
-        System.out.print("Enter variable a: ");
-        double varA = scanner.nextDouble();
-        
-        System.out.print("Enter variable b: ");
-        double varB = scanner.nextDouble();
-        
-       System.out.print("Enter variable c: ");
-       double varC = scanner.nextDouble();
-       
-       double topLine = varB * -1;
-       double BSquared = varB*varB;
-      
-       double squareRoot = BSquared - (4*varA*varC);
-       double squareRooted = Math.sqrt(squareRoot);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter variable a: ");
+            double varA = scanner.nextDouble();
+            
+            System.out.print("Enter variable b: ");
+            double varB = scanner.nextDouble();
+            
+            System.out.print("Enter variable c: ");
+            double varC = scanner.nextDouble();
+
+            double squareRoot = Math.sqrt((varB*varB) - (4*varA*varC));
+            double bottom = varA*2;
  
-       double bottom = varA*2; 
-      
-       
-       double plusFinal = (topLine + squareRooted)/bottom;
-       double minusFinal = (topLine - squareRooted)/bottom;
-       
-       boolean minusIsnotANum = Double.isNaN(plusFinal);
-       boolean plusIsnotANum = Double.isNaN(minusFinal);
-       
-       if (minusIsnotANum||plusIsnotANum){
-           System.out.println("Graph does not cross the Y axis");
-       } else {
-           System.out.println("plusFinal " + plusFinal);
-           System.out.println("minusFinal " + minusFinal);
-       };
-       
-      
-        scanner.close();
+            double plusFinal = ((varB*-1) + squareRoot)/bottom;
+            double minusFinal = ((varB*-1) - squareRoot)/bottom; 
+            
+            if (varC>=0.0 && varA>=0.0 || varA<=0.0 && varC<=0.0){
+                System.out.println("A and C cannot be positive/negative at the same time");
+            } else {
+                System.out.printf( "x= %.3f\n", plusFinal);
+                System.out.println("x= " + minusFinal);
+            }
+        }
     }
 }
